d20de189327f583e5c0650502070aec8298a7c94
yevheniiskakun <yevhenii.skakun@gmail.com>
2024-01-30 19:37:01 +0100

    Installed and integrated django_distill

--- productlist/pages/urls.py
+++ productlist/pages/urls.py
@@ -4,13 +4,14 @@ from . import views
 
 from django.conf import settings
 from django.conf.urls.static import static
+from django_distill import distill_path
 
 app_name = 'pages'
 
 urlpatterns = [
 	# main view
-	path('', views.index, name='home'),
-	path('delete_product/<int:id>', views.delete_product, name='delete_product'),
-	path('mark_product/<int:id>', views.mark_product, name='mark_product'),
-	path('add_product', views.add_product, name='add_product'),
+	distill_path('', views.index, name='home'),
+	distill_path('delete_product/<int:id>', views.delete_product, name='delete_product'),
+	distill_path('mark_product/<int:id>', views.mark_product, name='mark_product'),
+	distill_path('add_product', views.add_product, name='add_product'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
